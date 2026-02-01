277534f98f17d7b90c6be56f8daa25785943a67e
Alex <alexa.exebio@gmail.com>
2026-02-01 00:44:10 +0000

    listi

--- src/models.py
+++ src/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
@@ -16,4 +17,60 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Character(db.Model):#representa mi tabla personajes de mi base de datos
+    __tablename__ = 'characters'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False) 
+    quote: Mapped[str] =  mapped_column(String(200), nullable=False) 
+    image:  Mapped[str] = mapped_column(String(255), nullable=False) 
+    
+    def serialize(self): #método    
+        return {
+
+            "id": self.id,
+            "name": self.name,
+            "quote": self.quote,
+            "image": self.image
+
+        }
+  
+
+class Location(db.Model):#representa mi tabla Lugares de mi base de datos
+    __tablename__ = 'locations'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    quote: Mapped[str] = mapped_column(String(200), nullable=False)
+    image:  Mapped[str] = mapped_column(String(255), nullable=False)
+
+    
+    
+    def serialize(self): #método    
+        return {
+
+            "id": self.id,
+            "name": self.name,
+            "quote": self.quote,
+            "image": self.image
+        }
+    
+
+class Favorite(db.Model):   # representa mi tabla de favoritos del user
+    __tablename__ = 'Favorites'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[str] = mapped_column(db.ForeignKey('users.id'), nullable=False)
+    character_id: Mapped[str] = mapped_column(db.ForeignKey('characters.id'))
+    location_id: Mapped[str] = mapped_column(db.ForeignKey('locations.id'))
+    
+    
+    def serialize(self): #método    
+        return {
+
+            "id": self.id,
+            "name": self.name,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "location_id": self.location_id
         }
