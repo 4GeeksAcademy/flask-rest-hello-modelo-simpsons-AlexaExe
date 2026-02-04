05dd46b3afdada090ba7c63dadfa30f8e512d5b4
Alex <alexa.exebio@gmail.com>
2026-02-04 17:16:43 +0000

    he metido cambios de la api rest

--- src/models.py
+++ src/models.py
@@ -1,31 +1,73 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from flask_sqlalchemy import SQLAlchemy #importa la librería SQLAlchemy para que Flask conecte 
+#con una base de datos.
+from sqlalchemy import String, Boolean, ForeignKey, Table, Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase #esto es para declarar columnas en SQLAlchemy versión 2.0.
+from eralchemy2 import render_er #para cambiar el diagrama porque no aparecía más que el de base
 
-db = SQLAlchemy()
+class Base(DeclarativeBase):
+    pass
 
-class User(db.Model):
+db = SQLAlchemy(model_class = Base) #para conectar con el appy gestionar la base de datos
+
+#En caso de que la tabla se llame diferente el nombre de la clase colocar >>" __tablename__ = 'users'"
+
+# TABLA INTERMEDIA PARA NO GENERAR OTRO MODELO,además las tablas intermedias
+# existen porque la relación es de muchos a muchos
+
+
+favorites_table_characters = Table(
+    "favorites_characters",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id"),primary_key=True),
+    Column("character_id", ForeignKey("characters.id"), primary_key=True)
+)
+
+favorites_table_locations = Table(
+    "favorites_locations",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id"),primary_key=True),
+    Column("locations_id", ForeignKey("locations.id"), primary_key=True)
+)
+
+
+
+
+class User(db.Model): #representa mi tabla user de mi base de datos
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False) #yo he agregado esto como en el ejemplo
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favorite_characters: Mapped[list["Characters"]] = relationship("Characters",
+    secondary=favorites_table_characters, back_populates=
+    "favorite_by")
+    favorite_locations: Mapped[list["Location"]] = relationship("Location",
+    secondary=favorites_table_locations, back_populates=
+    "favorite_by")
 
-
-    def serialize(self):
+    def serialize(self): #método    
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            
             # do not serialize the password, its a security breach
         }
 
 
-class Character(db.Model):#representa mi tabla personajes de mi base de datos
+class Characters(db.Model):#representa mi tabla personajes de mi base de datos
     __tablename__ = 'characters'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False) 
-    quote: Mapped[str] =  mapped_column(String(200), nullable=False) 
-    image:  Mapped[str] = mapped_column(String(255), nullable=False) 
+    quote: Mapped[str]
+
+
+    favorite_by: Mapped[list["User"]] = relationship(
+        "User",
+        secondary=favorites_table_characters,
+        back_populates="favorite_characters"
+    )
     
     def serialize(self): #método    
         return {
@@ -33,19 +75,23 @@ class Character(db.Model):#representa mi tabla personajes de mi base de datos
             "id": self.id,
             "name": self.name,
             "quote": self.quote,
-            "image": self.image
+            "favorite_by":[user.id for user in self.favorite_by]
 
         }
-  
+    
+
 
 class Location(db.Model):#representa mi tabla Lugares de mi base de datos
     __tablename__ = 'locations'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
-    quote: Mapped[str] = mapped_column(String(200), nullable=False)
-    image:  Mapped[str] = mapped_column(String(255), nullable=False)
+    quote: Mapped[str]
 
-    
+    favorite_by: Mapped[list["User"]] = relationship(
+        "User",
+        secondary=favorites_table_locations,
+        back_populates="favorite_locations"
+    )
     
     def serialize(self): #método    
         return {
@@ -53,24 +99,9 @@ class Location(db.Model):#representa mi tabla Lugares de mi base de datos
             "id": self.id,
             "name": self.name,
             "quote": self.quote,
-            "image": self.image
-        }
-    
+            "favorite_by":[user.id for user in self.favorite_by]
 
-class Favorite(db.Model):   # representa mi tabla de favoritos del user
-    __tablename__ = 'Favorites'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[str] = mapped_column(db.ForeignKey('users.id'), nullable=False)
-    character_id: Mapped[str] = mapped_column(db.ForeignKey('characters.id'))
-    location_id: Mapped[str] = mapped_column(db.ForeignKey('locations.id'))
-    
-    
-    def serialize(self): #método    
-        return {
-
-            "id": self.id,
-            "name": self.name,
-            "user_id": self.user_id,
-            "character_id": self.character_id,
-            "location_id": self.location_id
         }
+
+
+render_er(db.Model.metadata, 'diagram.png')
